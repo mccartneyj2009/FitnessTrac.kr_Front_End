4f19b9852da6b65573978b1b5802fec58863a2d1
Emma Ruth Nagaño <emmaruthnagano@Emmas-MacBook-Pro.local>
2022-03-25 03:40:11 -0400

    designed routines

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,29 +1,39 @@
+import "./css/Routines.css";
+
 const Routines = ({ routines, setRoutines }) => {
   // console.log(routines);
   if (!routines) {
     return <div></div>;
   }
-
+console.log(routines)
   return (
-    <>
+    <div className="routines_main">
+
       <h1>Routines</h1>
+
       {routines.map((routine) => (
-        <div key={routines.id} id="routines">
-          <h2>{routine.name}</h2>
-          <h3>Goal: {routine.goal}</h3>
-          <h3>Author: {routine.creatorName}</h3>
-          {/* maybe map through activities */}
+        <div key={routines.id} id="routines" className="routines_container">
+
+          <h2>{routine.name.toUpperCase()}</h2>
+          <div className="routines_creator"><span>{routine.creatorName}</span></div>
+          
           {routine.activities.map((activity) => (
-            <div key={routine.id}>
-              <h3>Activities: {activity.name}</h3>
-              <p>Description: {activity.description}</p>
-              <h4>Count: {activity.count}</h4>
-              <h4>Duration: {activity.duration}</h4>
+            <div key={activity.id} className="routines_activities_content">
+
+              <p className="routines_activities_name">{activity.name}</p>
+              <p><span>Count: </span>{activity.count}</p>
+              <p><span>Duration: </span>{activity.duration}</p>
+              <p>{activity.description}</p>
+
             </div>
           ))}
+
+          <h4 className="routines_goal">Goal: {routine.goal}</h4>
+          
         </div>
       ))}
-    </>
+      
+    </div>
   );
 };
 
